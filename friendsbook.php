572690927acc774f84a56a3873ee1731cde78f49
Kalidas Sake <kalidasake07@gmail.com>
2018-02-07 01:05:15 +0530

    popus
    new popup option included

--- friendsbook.php
+++ friendsbook.php
@@ -21,35 +21,12 @@ else{
 <html>
 <head>
     <title>Friendsbook Home</title>
-    <style>
-        .home-div{
-            float: right;
-            display: none;
-        }
-
-        .profile-div{
-            float: right;
-            
-        }
-    </style>
 </head>
 <body>
     <h2>Site under construction!</h2>
     <?php
     require_once 'header.php';
     ?>
-    <div class="home-div">
-        <?php
-        include_once 'home.php';
-        ?>
-    </div>
-
-    <div class="profile-div">
-        <?php
-        include_once 'profile.php';
-        ?>
-    </div>
-
 
 </body>
 </html>
--- header.php
+++ header.php
@@ -3,9 +3,22 @@
     <nav>
         <ul>
             <li><a href="home.php" >Home</a> </li>
-            <li><a href="profile.php" >Profile</a> </li>
+            <li><a href="" >Profile</a> </li>
             <li><a href="#" >Inbox</a> </li>
             <li><a href="#" >Notifications</a> </li>
         </ul>
     </nav>
 </div>
+
+<style>
+    div{
+        background-color: #cae8ca;
+    }
+
+    li{
+        float: left;
+        list-style-type: none;
+
+    }
+
+</style>
